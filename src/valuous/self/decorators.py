565c945b07ca6b13d640aed3decd8039c001a133
Daniel Sosebee <dnsosebee@gmail.com>
2024-08-19 16:56:48 -0600

    Auto-commit

--- src/valuous/self/decorators.py
+++ src/valuous/self/decorators.py
@@ -12,6 +12,7 @@ def trace(goal: str = "unknown"):
             info = {"module": func.__module__, "name": func.__name__,
                     "goal": goal, "args": args, "kwargs": kwargs, "result": result}
             logging.info(f"Trace: {info}")
+            print('here')
             return result
         return wrapper
     return decorator
