f93eb9c4eb4e066bbf9b9737a44a763b4b8c2f75
Daniel Sosebee <dnsosebee@gmail.com>
2024-08-19 16:59:53 -0600

    init logger

--- src/valuous/peripherals/log.py
+++ src/valuous/peripherals/log.py
@@ -1,22 +1,25 @@
 import logging
 
-# Create a custom logger
-logger = logging.getLogger('my_logger')
-logger.setLevel(logging.DEBUG)
 
-# Create handlers
-console_handler = logging.StreamHandler()
-file_handler = logging.FileHandler('app.log')
+def init_logger():
 
-console_handler.setLevel(logging.DEBUG)
-file_handler.setLevel(logging.INFO)
+    # Create a custom logger
+    logger = logging.getLogger('my_logger')
+    logger.setLevel(logging.DEBUG)
 
-# Create formatters and add them to handlers
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
+    # Create handlers
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler('app.log')
 
-# Add handlers to the logger
-# logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+    console_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(logging.INFO)
+
+    # Create formatters and add them to handlers
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # Add handlers to the logger
+    # logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
