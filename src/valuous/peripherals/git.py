4dd659eb9b992f29f65f9b8df0bad2962e9d1b32
Daniel Sosebee <dnsosebee@gmail.com>
2024-08-19 16:48:18 -0600

    try

--- src/valuous/peripherals/git.py
+++ src/valuous/peripherals/git.py
@@ -2,26 +2,29 @@ import subprocess
 
 
 def get_porcelain_status():
-    return subprocess.run(['git', 'status', '--porcelain'], text=True, stdout=subprocess.PIPE).stdout
+    return subprocess.run(['git', 'status', '--porcelain'], text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
 
 
 def commit_all(message="Auto-commit"):
-    subprocess.run(['git', 'add', '.'], stdout=subprocess.DEVNULL)
-    subprocess.run(['git', 'commit', '-m', message], stdout=subprocess.DEVNULL)
+    subprocess.run(['git', 'add', '.'],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(['git', 'commit', '-m', message],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def fetch_branch(branch_name='user'):
     subprocess.run(['git', 'fetch', 'origin', branch_name],
-                   stdout=subprocess.DEVNULL)
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def merge(branch_name='user'):
-    return subprocess.run(['git', 'merge', f'origin/{branch_name}'], stdout=subprocess.DEVNULL)
+    return subprocess.run(['git', 'merge', f'origin/{branch_name}'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def abort_merge():
-    return subprocess.run(['git', 'merge', '--abort'], stdout=subprocess.DEVNULL)
+    return subprocess.run(['git', 'merge', '--abort'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def push():
-    subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL)
+    subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
