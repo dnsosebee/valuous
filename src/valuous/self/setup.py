4f255d6cb7ffeb84524ece4e6b88621127ba31ae
Daniel Sosebee <dnsosebee@gmail.com>
2024-08-20 09:03:49 -0600

    Auto-commit

--- src/valuous/self/setup.py
+++ src/valuous/self/setup.py
@@ -1,6 +1,7 @@
 # src/valuous/self/main.py - This is the execution entrypoint to valuous, executed by the environment on startup.
 
 
+from valuous.peripherals import google
 from valuous.peripherals.log import init_logger
 from valuous.self.decorators import trace
 from valuous.self.loop import loop
@@ -13,5 +14,6 @@ def setup():
 @trace(goal="Be a corrigible agent. Provide long-horizon value to the user by continuously attempting to understand the user's long-term preferences and adapting yourself to maximally help them. Improve yourself as needed in the process.")
 def start_loop():
     init_logger()
+    gmail_service = google.get_gmail_service()
     while True:
         loop()
