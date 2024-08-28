168530077fa216503ce967647d9bf26e38c4d304
Daniel Sosebee <dnsosebee@gmail.com>
2024-08-28 11:27:07 -0600

    Auto-commit

--- src/valuous/self/loop.py
+++ src/valuous/self/loop.py
@@ -53,7 +53,8 @@ def loop():
     sync.sync_git()
 
     for browser in workspace:
-        browser["response"] = browser["tool"].func(browser["args"])
+        browser["response"] = browser["tool"].func(
+        ) if browser["args"] is None else browser["tool"].func(browser["args"])
         if "redirect" in browser["response"]:
             browser["tool"] = browser["response"]["redirect"]["tool"]
             browser["args"] = browser["response"]["redirect"]["args"]
