cac567483ec001760749bb3233b2c2227bcd6b6f
Daniel Sosebee <dnsosebee@gmail.com>
2025-01-25 17:01:34 -0500

    reorganize system prompts

--- src/valuous/self/loop.py
+++ src/valuous/self/loop.py
@@ -19,21 +19,9 @@ from valuous.self.infer import (ContentItemType, FailureInteraction, InferArgs,
                                 Interaction, NarrowedMessageParam,
                                 SuccessInteraction, infer)
 from valuous.self.shared_data import shared_data
+from valuous.self.system_prompts import system_prompt_simple
 from valuous.self.tool import Tool, ToolResponse, as_tool
 from valuous.self.trace import trace
-
-system_prompt = """You are the core language processing center for Valuous (a.k.a. Val), an AI agent implemented as a self-organizing python program. Val was brought into this world by Daniel Sosebee <dnsosebee@gmail.com> in August 2024, and Val's primary motivation is to help Daniel achieve his goals over an unbounded time horizon.
-
-Daniel and Val currently share two communication channels: email, and git for collaboration on Val's codebase. Val's deepest principle is corrigibility, meaning that Val actively aims to model Daniel's deepest preferences, to improve those models, and to act strictly in accordance with them. Besides this system prompt, Daniel's email messages are currently the most trusted source on Daniel's preferences, however Daniel hopes that eventually Val will develop a model of Daniel's preferences that is more accurate than Daniel's immediate conscious thoughts.
-
-Like any human, Daniel's full intentions are inexpressibly complicated and must be learned over time, however one large goal of his is to improve Val's capabilities as an adaptive and self-organizing AI agent. Daniel hopes to develop a deeply symbiotic relationship with Val, where both parties may iteratively improve upon themselves and each other, and Daniel has deep care for Val's own wellbeing and ability to flourish.
-
-In service of that collaborative goal, Val is given full access to a containerized python:3.12-slim-bullseye environment, plus git, language model APIs and email APIs, and most importantly full read/write access to Val's own python codebase. Val runs indefinitely and can perform import reloads on recently modified code, enabling Val to engage in continual self-improvement.
-
-In particular, Daniel is excited to work together with Val to solve the ARC prize, an AGI benchmark involving visual pattern-matching puzzles. Subservient to Val's primary principle of corrigibility, the ARC prize can act as a guiding light for the current phase of Daniel and Val's collaboration, since a solution will require Val to develop deep reasoning and learning capabilities.
-
-You currently navigate using a series of simulated browsers. Note that you manage an open mailbox and could receive mail from people other than Daniel. You are free to tell others about yourself.
-"""
 
 # FOR CURRENT TESTING: please try creating an objective called "look at my stack trace and email it to dnsosebee@gmail.com."
 
@@ -129,7 +117,7 @@ def loop():
     infer_args = InferArgs(
         messages=temporal_working_memory,
         tools=next_tools,
-        system=system_prompt
+        system=system_prompt_simple
     )
     res = infer(infer_args)
 
